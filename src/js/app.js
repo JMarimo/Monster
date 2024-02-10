1389d41b86de90bb6d9e5e18b4659935fe807dce
Jane Marimo <jenny.bulatova@gmail.com>
2024-02-10 18:47:34 +0300

    Переделала все к чертям

--- src/js/app.js
+++ src/js/app.js
@@ -1,27 +1,13 @@
+/* eslint-disable no-unused-vars */
+import Board from '../Board';
+
+const fields = Array.from(document.querySelectorAll('.field'));
+
+const board = new Board(fields);
+
 document.addEventListener('DOMContentLoaded', () => {
-  const goblin = document.querySelectorAll('.container-row');
-  const arrGoblins = [];
-  const arrToGenerate = [];
-
-  goblin.forEach((item) => arrGoblins.push(item));
-
-  function getRandomInt() {
-    const min = Math.ceil(0);
-    const max = Math.floor(15);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
-
-  function generationPosition() {
-    const numberGenerate = getRandomInt();
-    if (numberGenerate !== arrToGenerate[arrToGenerate.length]) {
-      arrGoblins.forEach((item) => item.classList.remove('container-goblin'));
-      arrGoblins[numberGenerate].classList.add('container-goblin');
-      arrToGenerate.push(numberGenerate);
-    }
-  }
-  generationPosition();
-
-  setInterval(() => {
-    generationPosition();
+  const interval = setInterval(() => {
+    board.deleteActiveClass();
+    board.addActiveClass();
   }, 1000);
 });
